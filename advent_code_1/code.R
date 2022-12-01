c7c8ceb66796f67948817dd8a106c7fe89657441
James Boot <44063127+jamesboot@users.noreply.github.com>
2022-12-01 21:00:07 +0000

    Add files via upload

---
+++ advent_code_1/code.R
@@ -0,0 +1,56 @@
+# Day 1
+setwd('Documents/Code_Advent/advent_code_1/')
+
+# Read in file 
+# Make sure to leave in blanks
+input <- read.delim(file = 'input.txt',
+                    header = F,
+                    blank.lines.skip = F)
+
+# Convert input to vector
+input <- as.vector(input$V1)
+
+# Make a blank results vector
+results <- c()
+
+# Make count object equal to 0
+count <- 0
+
+# For loop, go through each element in input vector
+# if count is 0 make the element the count
+# if count is greater and there is no NA - add to count
+# if count is greater than 0 and there is an NA, put count in results and reset count
+for (x in 1:length(input)) {
+  
+  if (count > 0 & is.na(input[x]) == F) {
+    count <- count + input[x]
+    
+  } else if (count > 0 & is.na(input[x]) == T) {
+    results[x] <- count
+    count <- 0
+    
+  } else if (count == 0) {
+    count <- input[x]
+    
+  }
+  
+}
+
+# Get all the counts 
+final <- na.omit(results)
+
+# Find max value - answer 1
+max(final)
+
+# Find top 3 values and add together - answer 2
+sum(sort(final, decreasing = T)[1:3])
+
+
+
+
+
+
+  
+  
+  
+  
